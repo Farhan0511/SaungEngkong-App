12d2313284bd557770d6c26edb4ac170b7258024
farhan0511 <programingsukses@gmail.com>
2026-05-11 06:04:12 +0700

    add component About, Menu, Hero

---
+++ resources/views/user/components/About-Component.blade.php
@@ -0,0 +1,63 @@
+  <section id="about" class="about section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+          <h2>Tentang Kami</h2>
+          <p><span>Kenal Lebih Dekat</span> <span class="description-title">SaungEngkong</span></p>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+
+          <div class="row gy-4">
+              <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
+                  <img src="{{ asset('assets/user/img/about.jpg') }}" class="img-fluid mb-4" alt="SaungEngkong">
+
+                  <div class="book-a-table">
+                      <h3>Reservasi Meja</h3>
+                      <p>+62 812 3456 7890</p>
+                  </div>
+              </div>
+
+              <div class="col-lg-5" data-aos="fade-up" data-aos-delay="250">
+                  <div class="content ps-0 ps-lg-5">
+
+                      <p class="fst-italic">
+                          SaungEngkong hadir sebagai tempat makan nyaman dengan suasana tradisional
+                          yang dipadukan dengan cita rasa khas Nusantara.
+                      </p>
+
+                      <ul>
+                          <li>
+                              <i class="bi bi-check-circle-fill"></i>
+                              <span>Menyajikan makanan berkualitas dengan bahan pilihan terbaik.</span>
+                          </li>
+
+                          <li>
+                              <i class="bi bi-check-circle-fill"></i>
+                              <span>Suasana saung yang hangat dan cocok untuk keluarga maupun teman.</span>
+                          </li>
+
+                          <li>
+                              <i class="bi bi-check-circle-fill"></i>
+                              <span>Pelayanan ramah dan pengalaman kuliner yang nyaman untuk setiap pengunjung.</span>
+                          </li>
+                      </ul>
+
+                      <p>
+                          Kami percaya bahwa makanan bukan hanya soal rasa, tetapi juga tentang
+                          kebersamaan dan kenyamanan. SaungEngkong menjadi tempat yang tepat untuk
+                          menikmati hidangan lezat sambil bersantai bersama orang terdekat.
+                      </p>
+
+                      <div class="position-relative mt-4">
+                          <img src="{{ asset('assets/user/img/about-2.jpg') }}" class="img-fluid"
+                              alt="Tentang SaungEngkong">
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+
+      </div>
+
+  </section>
